37514ecf34fc201888d5695e59109ce7f74c4536
Michael Cole <michaelcole239@gmail.com>
2021-09-19 23:53:38 +0100

    Collect plots, add ellipse

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import numpy as np
 import plotly.express as px
+from plotly.subplots import make_subplots
 
 from darts.darts import Dartboard
 st.set_page_config(layout="wide")
 st.title('Dartboard')
 
-db = Dartboard(pixels=301)
-
-fig = px.imshow(db.db_score_map, origin='lower', width=1000, height=1000)
-fig.update_xaxes(showticklabels=False)
-fig.update_yaxes(showticklabels=False)
-fig.update(layout_coloraxis_showscale=False)
-st.plotly_chart(fig, use_container_width=True)
+PIXELS = 301
+db = Dartboard(pixels=PIXELS)
 
 st.subheader('Expected score map')
+
+nonspherical = st.checkbox('Non-spherical distribution?')
+if nonspherical:
+    tilted = st.checkbox('Tilted Gaussian distribution?')
 
 with st.form('Parameters:') as form:
     mean_cols = st.columns(2)
@@ -24,12 +24,10 @@ with st.form('Parameters:') as form:
         mu_y = st.number_input('y mean', min_value=-200.0, max_value=200.0, value=0.0)
     mu = np.array([mu_x, mu_y])
 
-    nonspherical = st.checkbox('Non-spherical distribution?')
     if not nonspherical:
         sigma = st.number_input('Throw variance', value=50.0)
         Sigma = sigma*np.eye(2)
     else:
-        tilted = st.checkbox('Tilted Gaussian distribution?')
         if not tilted:
             sigma_x = st.number_input('x variance', value=50.0)
             sigma_y = st.number_input('y variance', value=50.0)
@@ -45,13 +43,24 @@ with st.form('Parameters:') as form:
             Sigma = np.array([[sigma_11, sigma_12], [sigma_21, sigma_22]])
     st.form_submit_button()
 
+fig = px.imshow(db.db_score_map, origin='lower', height=1000)
+fig.update_xaxes(showticklabels=False)
+fig.update_yaxes(showticklabels=False)
+aim_center = [int(PIXELS/2), int(PIXELS/2)]
+throw_stds = [np.sqrt(Sigma[0, 0]), np.sqrt(Sigma[1, 1])]
+fig.add_shape(type='circle',
+              x0=aim_center[0]+throw_stds[0],
+              y0=aim_center[1]-throw_stds[1],
+              x1=aim_center[0]-throw_stds[0],
+              y1=aim_center[1]+throw_stds[1],
+              line_color='red',
+              fillcolor='red', opacity=0.5)
+st.plotly_chart(fig, use_container_width=True)
+
 exp_score_map = db.exp_score_map(mu, Sigma, padding=100)
 exp_fig = px.imshow(db.exp_score_map(mu, Sigma, padding=100),
-                    origin='lower',
-                    width=1000,
-                    height=1000)
+                    origin='lower', height=1000)
 exp_fig.update_xaxes(showticklabels=False)
 exp_fig.update_yaxes(showticklabels=False)
-exp_fig.update(layout_coloraxis_showscale=False)
 
 st.plotly_chart(exp_fig, use_container_width=True)
